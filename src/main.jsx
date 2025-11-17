6a574869f5624937932641f5ba33d4981b8440b1
ji009868 <ji009868@naver.com>
2025-11-17 14:31:33 +0900

    fix: base & basename for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'; // 라우터를 사용하기 위해 추가
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/react-portfolio/"> 
-      {/* 라우터로 앱을 감싸기 */}
-      {/* basename에 Repository 경로를 추가 */}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/react-portfolio/">
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
