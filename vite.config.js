f5ad06551cb4e704b05dd2ce8c5fb8b4b8dcd091
ji009868 <ji009868@naver.com>
2025-11-24 12:49:44 +0900

    Build to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 // vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-portfolio/',   // ← 이미 있던 거 그대로 유지
+  base: '/react-portfolio/',     // GitHub 저장소 이름
   build: {
-    outDir: 'docs',            // ✅ dist 대신 docs 로 빌드
+    outDir: 'docs',              // ← 여기서 docs로 빌드
   },
 });
